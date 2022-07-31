2d32f5af1a35739558e4292c6d3616d956793a15
dhoine345 <lue42@yandex.ru>
2022-07-31 15:13:43 +0300

    add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,23 @@
 import './Header.css';
 import logo from '../../images/logo.svg'
+import { Link, NavLink } from 'react-router-dom';
 
-function Header() {
+function Header({ loggedIn }) {
   return (
     <header className="header">
-      <img src={logo} alt='Логотип.'/>
-      <nav className='header__nav'>
-        
+      <Link to='/' className='header__logo'/>
+      <nav className={loggedIn ? 'header__nav' : 'header__nav_disabled'}>
+        <NavLink to='/movies' className='header__navlink' activeClassName='header__navlink_active'>Фильмы</NavLink>
+        <NavLink to='/saved-movies' className='header__navlink' activeClassName='header__navlink_active'>Сохраненные фильмы</NavLink>
       </nav>
+      {!loggedIn ?
+      <nav className='header__auth'>
+        <Link to='/signup'>Регистрация</Link>
+        <Link to='/signin'>Войти</Link>
+      </nav>
+      :
+      <Link to='/profile'>Аккаунт</Link>
+      }
     </header>
   );
 }
