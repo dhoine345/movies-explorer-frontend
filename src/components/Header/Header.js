495d7a4cbc0fd7c74966d0de784120adfbf3362f
dhoine345 <lue42@yandex.ru>
2022-08-01 14:12:15 +0300

    add Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,37 @@
 import './Header.css';
-import logo from '../../images/logo.svg'
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useHistory } from 'react-router-dom';
 
 function Header({ loggedIn }) {
+  function handleMouseEnter(e) {
+    if (!e.target.classList.contains('header__link_active'))
+      e.target.classList.add('header__link_active')
+      e.target.nextElementSibling ? e.target.nextElementSibling.classList.remove('header__link_active')
+      : e.target.previousElementSibling.classList.remove('header__link_active')
+  }
+
+  const pathLoginOrRegist = useHistory().location.pathname === '/login' || '/register';
+
   return (
-    <header className="header">
+    <header className={`header ${loggedIn && 'header_white-back'}`}>
       <Link to='/' className='header__logo'/>
-      <nav className={loggedIn ? 'header__nav' : 'header__nav_disabled'}>
-        <NavLink to='/movies' className='header__navlink' activeClassName='header__navlink_active'>Фильмы</NavLink>
+      {pathLoginOrRegist && <h2 className='header__title'>Добро пожаловать!</h2>}
+      {loggedIn ?
+      <>
+      <nav className='header__nav'>
+        <NavLink to='/movies' className='header__navlink' activeClassName='header__navlink_active' onMouseEnter={handleMouseEnter}>Фильмы</NavLink>
         <NavLink to='/saved-movies' className='header__navlink' activeClassName='header__navlink_active'>Сохраненные фильмы</NavLink>
       </nav>
-      {!loggedIn ?
-      <nav className='header__auth'>
-        <Link to='/signup'>Регистрация</Link>
-        <Link to='/signin'>Войти</Link>
-      </nav>
+      <Link to='/profile' className='header__account-container'>
+        Аккаунт
+        <div className='header__account-back'><div className='header__account-svg' /></div>
+      </Link>
+      <button type='button' className='header__hamburger-button' />
+      </>
       :
-      <Link to='/profile'>Аккаунт</Link>
+      <nav className='header__auth'>
+        <Link to='/signup' className='header__link' onMouseEnter={handleMouseEnter} >Регистрация</Link>
+        <Link to='/signin' className='header__link header__link_active' onMouseEnter={handleMouseEnter} >Войти</Link>
+      </nav>
       }
     </header>
   );
