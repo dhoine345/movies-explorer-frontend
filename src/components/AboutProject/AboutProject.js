8015ca3fbc603520ab21fa004510248f69228d9e
dhoine345 <lue42@yandex.ru>
2022-08-03 16:46:23 +0300

    fix tag

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -7,11 +7,11 @@ function AboutProject() {
       <ul className='project__description'>
         <li className='project__article'>
           <h3 className='project__article-title'>Дипломный проект включал 5 этапов</h3>
-          <p3 className='project__article-text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p3>
+          <p className='project__article-text'>Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
         </li>
         <li className='project__article'>
           <h3 className='project__article-title'>На выполнение диплома ушло 5 недель</h3>
-          <p3 className='project__article-text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p3>
+          <p className='project__article-text'>У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
         </li>
       </ul>
       <ul className='project__table'>
