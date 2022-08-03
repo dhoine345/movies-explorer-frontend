fdebb6423f116ff41afd0353dda443898c9baf89
dhoine345 <lue42@yandex.ru>
2022-08-03 14:22:25 +0300

    fix elemets names

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -6,14 +6,14 @@ function Techs() {
       <h2 className='section-title'>Технологии</h2>
       <h3 className='techs__subtitle'>7 технологий</h3>
       <p className='techs__description'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
-      <ul className='techs__table'>
-        <li className='techs__table-cell'>HTML</li>
-        <li className='techs__table-cell'>CSS</li>
-        <li className='techs__table-cell'>JS</li>
-        <li className='techs__table-cell'>React</li>
-        <li className='techs__table-cell'>Git</li>
-        <li className='techs__table-cell'>Express.js</li>
-        <li className='techs__table-cell'>mongoDB</li>
+      <ul className='techs__list'>
+        <li className='techs__list-item'>HTML</li>
+        <li className='techs__list-item'>CSS</li>
+        <li className='techs__list-item'>JS</li>
+        <li className='techs__list-item'>React</li>
+        <li className='techs__list-item'>Git</li>
+        <li className='techs__list-item'>Express.js</li>
+        <li className='techs__list-item'>mongoDB</li>
       </ul>
     </section>
   )
