524822f86470a266cf4123e3443a3ba8a80a2465
dhoine345 <lue42@yandex.ru>
2022-08-04 19:30:06 +0300

    fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import './Header.css';
 import { Link, NavLink, useLocation } from 'react-router-dom';
-import { useState, useEffect } from "react";
 
 function Header({ loggedIn }) {
   function handleMouseEnter(e) {
@@ -13,27 +12,26 @@ function Header({ loggedIn }) {
   const path = useLocation().pathname;
 
   return (
-    <header className={`${((path === '/signin') || (path === '/signup')) ? 'header__reg-or-log' : 'header'} ${loggedIn && 'header_white-back'}`}>
+    <header className={`${((path === '/signin') || (path === '/signup')) ? 'header__reg-or-log' : 'header'}`}>
       <Link to='/' className='header__logo'/>
       {((path === '/signin') || (path === '/signup')) && <h2 className='header__title'>{path === '/signup' ? 'Добро пожаловать!' : 'Рады видеть!'}</h2>}
-      {path === '/' ? (loggedIn ?
+      {((path === '/movies') || (path === '/saved-movies')) &&
       <>
-      <nav className='header__nav'>
-        <NavLink to='/movies' className='header__navlink' activeClassName='header__navlink_active'>Фильмы</NavLink>
-        <NavLink to='/saved-movies' className='header__navlink' activeClassName='header__navlink_active'>Сохраненные фильмы</NavLink>
-      </nav>
-      <Link to='/profile' className='header__account-container'>
-        Аккаунт
-        <div className='header__account-back'><div className='header__account-svg' /></div>
-      </Link>
-      <button type='button' className='header__hamburger-button' />
-      </>
-      :
-      <nav className='header__auth'>
+        <nav className='header__nav'>
+          <NavLink to='/movies' className='header__navlink' activeClassName='header__navlink_active'>Фильмы</NavLink>
+          <NavLink to='/saved-movies' className='header__navlink' activeclassname='header__navlink_active'>Сохраненные фильмы</NavLink>
+        </nav>
+        <Link to='/profile' className='header__account-container'>
+          Аккаунт
+          <div className='header__account-back'><div className='header__account-svg' /></div>
+        </Link>
+        <button type='button' className='header__hamburger-button' />
+      </>}
+      {path === '/' &&
+        <nav className='header__auth'>
         <Link to='/signup' className='header__link' onMouseEnter={handleMouseEnter} >Регистрация</Link>
         <Link to='/signin' className='header__link header__link_active' onMouseEnter={handleMouseEnter} >Войти</Link>
-      </nav>)
-      : ''
+      </nav>
       }
     </header>
   );
---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+
+export const pathNames = {
+  signin: '/signin',
+  signup: '/signup',
+  movies: '/movies',
+  savedMovies: '/saved-movies',
+  profile: '/profile',
+  default: '/'
+}
