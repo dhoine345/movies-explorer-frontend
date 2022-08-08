06f34d1458213ec3a26c18dd0c756ade8ae3d98e
dhoine345 <lue42@yandex.ru>
2022-08-08 14:08:22 +0300

    fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,75 @@
 
-export const pathNames = {
-  signin: '/signin',
-  signup: '/signup',
-  movies: '/movies',
-  savedMovies: '/saved-movies',
-  profile: '/profile',
-  default: '/'
-}
+export const data = [
+  {
+    nameRu: '33 слова о дизайне',
+    duration: 104,
+    trailerLink: 'https://basik.ru/images/landscapes_walls/08_landscape.jpg',
+    _id: 1
+  },
+  {
+    nameRu: '33 слова о дизайне',
+    duration: 104,
+    trailerLink: '',
+    id: 2
+  },
+  {
+    nameRu: '33 слова о дизайне',
+    duration: 104,
+    trailerLink: '',
+    id: 3
+  },
+  {
+    nameRu: '33 слова о дизайне',
+    duration: 104,
+    trailerLink: '',
+    id: 4
+  },
+  {
+    nameRu: '33 слова о дизайне',
+    duration: 104,
+    trailerLink: '',
+    id: 5
+  },
+  {
+    nameRu: '33 слова о дизайне',
+    duration: 104,
+    trailerLink: '',
+    id: 6
+  },
+  {
+    nameRu: '33 слова о дизайне',
+    duration: 104,
+    trailerLink: '',
+    id: 7
+  },
+  {
+    nameRu: '33 слова о дизайне',
+    duration: 104,
+    trailerLink: '',
+    id: 8
+  },
+  {
+    nameRu: '33 слова о дизайне',
+    duration: 104,
+    trailerLink: '',
+    id: 9
+  },
+  {
+    nameRu: '33 слова о дизайне',
+    duration: 104,
+    trailerLink: '',
+    id: 10
+  },
+  {
+    nameRu: '33 слова о дизайне',
+    duration: 104,
+    trailerLink: '',
+    id: 11
+  },
+  {
+    nameRu: '33 слова о дизайне',
+    duration: 104,
+    trailerLink: '',
+    id: 12
+  }
+]
