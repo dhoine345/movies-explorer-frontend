ad3dcadf7d6d33ad3c2cd5779d6a8a4bbd30a79d
dhoine345 <lue42@yandex.ru>
2022-08-16 09:09:58 +0300

    add images

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -18,70 +18,70 @@ export const data = [
   {
     nameRu: '33 слова о дизайне',
     duration: 104,
-    trailerLink: '',
+    trailerLink: img1,
     favorite: false,
     id: 3
   },
   {
     nameRu: '33 слова о дизайне',
     duration: 104,
-    trailerLink: '',
+    trailerLink: img1,
     favorite: false,
     id: 4
   },
   {
     nameRu: '33 слова о дизайне',
     duration: 104,
-    trailerLink: '',
+    trailerLink: img1,
     favorite: false,
     id: 5
   },
   {
     nameRu: '33 слова о дизайне',
     duration: 104,
-    trailerLink: '',
+    trailerLink: img1,
     favorite: false,
     id: 6
   },
   {
     nameRu: '33 слова о дизайне',
     duration: 104,
-    trailerLink: '',
+    trailerLink: img1,
     favorite: false,
     id: 7
   },
   {
     nameRu: '33 слова о дизайне',
     duration: 104,
-    trailerLink: '',
+    trailerLink: img1,
     favorite: false,
     id: 8
   },
   {
     nameRu: '33 слова о дизайне',
     duration: 104,
-    trailerLink: '',
+    trailerLink: img1,
     favorite: false,
     id: 9
   },
   {
     nameRu: '33 слова о дизайне',
     duration: 104,
-    trailerLink: '',
+    trailerLink: img1,
     favorite: false,
     id: 10
   },
   {
     nameRu: '33 слова о дизайне',
     duration: 104,
-    trailerLink: '',
+    trailerLink: img1,
     favorite: false,
     id: 11
   },
   {
     nameRu: '33 слова о дизайне',
     duration: 104,
-    trailerLink: '',
+    trailerLink: img1,
     favorite: false,
     id: 12
   }
