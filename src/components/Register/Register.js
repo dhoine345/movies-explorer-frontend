f30ca9f6352436507ce849ca9349d241d9539a86
dhoine345 <lue42@yandex.ru>
2022-08-23 11:32:49 +0300

    add handleregister

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,21 @@
 import './Register.css'
 import Form from '../Form/Form';
+import { api } from '../../utils/MainApi';
+import { useNavigate } from 'react-router-dom';
 
-function Register() {
+function Register({ isLoggedIn }) {
+  const history = useNavigate();
+  const handleRegister = (email, password, name) => {
+    api.register( email, password, name)
+      .then(() => {
+        api.login(email, password)
+          .then(() => {
+            isLoggedIn(true);
+            history('/movies');
+          })
+      })
+  }
+
   return (
     <Form
       typeOfForm={'register'}
@@ -10,6 +24,7 @@ function Register() {
       path='/signin'
       buttontext='Зарегистрироваться'
       greeting='Добро пожаловать!'
+      onRegister={handleRegister}
     />
   )
 }
