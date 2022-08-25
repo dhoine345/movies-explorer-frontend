17e1cef78d2af9378e938f69fb1819e115f146ac
dhoine345 <lue42@yandex.ru>
2022-08-25 09:28:46 +0300

    add render of saved movies

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,33 +1,37 @@
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
+import { useState, useEffect } from "react";
+import { useLocation } from 'react-router-dom';
+import { api } from '../../utils/MainApi';
 
-function MoviesCardList({ data, isSavedMovies, handlerAddButton, isAddButton }) {
+function MoviesCardList({ data, handlerAddButton, isAddButton }) {
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const location = useLocation().pathname;
+
+  const getSavedMovies = () => {
+    api.getFavoriteMovies()
+      .then(res => setFavoriteMovies(res.data))
+  }
+
+  useEffect(() => {
+    getSavedMovies();
+  }, []);
+
   return (
     <section className='moviescardlist'>
-      {!isSavedMovies ?
-        data.map((moviescard) => {
+      {
+        (data || favoriteMovies).map((moviescard) => {
           return (
             <MoviesCard
               moviescard={moviescard}
-              key={moviescard.id}
+              key={location === '/saved-movies' ? moviescard.movieId : moviescard.id}
+              renderSavedMovies={getSavedMovies}
+              favoriteMovies={favoriteMovies}
+              id={moviescard.movieId || moviescard.id}
             />
           )
         })
-      :
-        data.filter((moviescard) => {
-          return (
-            moviescard.favorite === true
-          )
-        }).map((moviescard) => {
-          return (
-            <MoviesCard
-              moviescard={moviescard}
-              key={moviescard.id}
-              isSavedMovies={isSavedMovies}
-            />
-          )
-        })
-    }
+      }
       <button
         className={`moviescardlist__button link-hover ${!isAddButton && 'moviescardlist__button_disabled'}`}
         onClick={handlerAddButton}
