7d14a9e75cf8343104ec079ae6df6b858b22405c
dhoine345 <lue42@yandex.ru>
2022-09-01 10:00:42 +0300

    fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,9 +7,15 @@ function Footer() {
       <div className='footer__container'>
         <div className='footer__copyright footer-text'>&copy; 2022</div>
         <ul className='footer__list'>
-          <li className='footer__list-item'><a className='footer__link footer-text link-hover' href='//practicum.yandex.ru/profile/web/' target='_blank' rel="noreferrer">Яндекс.Практикум</a></li>
-          <li className='footer__list-item'><a className='footer__link footer-text link-hover' href='//facebook.com/dhoine345' target='_blank' rel="noreferrer">Facebook</a></li>
-          <li className='footer__list-item'><a className='footer__link footer-text link-hover' href='//github.com/dhoine345' target='_blank' rel="noreferrer">Github</a></li>
+          <li className='footer__list-item'>
+            <a className='footer__link footer-text link-hover' href='//practicum.yandex.ru/profile/web/' target='_blank' rel="noreferrer">Яндекс.Практикум</a>
+          </li>
+          <li className='footer__list-item'>
+            <a className='footer__link footer-text link-hover' href='//facebook.com/dhoine345' target='_blank' rel="noreferrer">Facebook</a>
+          </li>
+          <li className='footer__list-item'>
+            <a className='footer__link footer-text link-hover' href='//github.com/dhoine345' target='_blank' rel="noreferrer">Github</a>
+            </li>
         </ul>
       </div>
     </footer>
